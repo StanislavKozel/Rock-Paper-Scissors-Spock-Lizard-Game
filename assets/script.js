cc83ac71631aeea79b5e33b04d487cd29f2621c6
StanislavKozel <s.kozel46@gmail.com>
2022-08-29 21:47:50 +0000

    add event listeners

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,39 @@ const userScore = 0;
 const compScore = 0;
 const userScoreSpan = document.getElementById("user-score");
 const compScoreSpan = document.getElementById("comp-score");
+const scoreDiv = document.getElementsByClassName("score");
+const results = document.getElementsByClassName("results");
+const rockDiv = document.getElementById("rock");
+const paperDiv = document.getElementById("paper");
+const scissorsDiv = document.getElementById("scissors");
+const spockDiv = document.getElementById("spock");
+const lizardDiv = document.getElementById("lizard");
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors", "spock", "lizard"];
+    console.log(Math.random() * 3);
+}
+
+function game(usersChoice) {
+
+}
+
+function main() {
+    rockDiv.addEventListener('click', function() {
+        game("rock");
+    })
+    paperDiv.addEventListener('click', function() {
+        game("paper");
+    })
+    scissorsDiv.addEventListener('click', function() {
+        game("scissors");
+    })
+    spockDiv.addEventListener('click', function() {
+        game("spock");
+    })
+    lizardDiv.addEventListener('click', function() {
+        game("lizard");
+    })
+}
+
+main();
