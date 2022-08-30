01212461548c810112354e686827bf959f8cca0f
StanislavKozel <s.kozel46@gmail.com>
2022-08-30 22:26:21 +0000

    add functions win, loose, draw

--- assets/script.js
+++ assets/script.js
@@ -3,85 +3,99 @@ let compScore = 0;
 const userScoreSpan = document.getElementById("user-score");
 const compScoreSpan = document.getElementById("comp-score");
 const scoreDiv = document.getElementsByClassName("score");
-const results = document.getElementsByClassName("results");
-const rockDiv = document.getElementById("rock");
-const paperDiv = document.getElementById("paper");
-const scissorsDiv = document.getElementById("scissors");
-const spockDiv = document.getElementById("spock");
-const lizardDiv = document.getElementById("lizard");
+const results = document.querySelector(".results > p");
+const rockDiv = document.getElementById("r");
+const paperDiv = document.getElementById("p");
+const scissorsDiv = document.getElementById("s");
+const spockDiv = document.getElementById("sp");
+const lizardDiv = document.getElementById("l");
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors", "spock", "lizard"];
+    const choices = ["r", "p", "s", "sp", "l"];
     const randomNumber = Math.floor(Math.random() * 5);
     return choices[randomNumber];
+} 
+
+function convertToWord(letter) {
+    if(letter === "r") return "Rock";
+    if(letter === "p") return "Paper";
+    if(letter === "s") return "Scissors";
+    if(letter === "sp") return "Spock";
+    return "Lizard";
 }
 
-function win() {
+function win(usersChoice, computerChoice) {
   userScore++;
   userScoreSpan.innerHTML = userScore;
   compScoreSpan.innerHTML = compScore;
+  results.innerHTML = `${convertToWord(usersChoice)} beats ${convertToWord(computerChoice)}. You Win!`;
 }
 
-function loose() {
-    console.log("lost")
+function loose(usersChoice, computerChoice) {
+  compScore++;
+  userScoreSpan.innerHTML = userScore;
+  compScoreSpan.innerHTML = compScore;
+  results.innerHTML = `${convertToWord(usersChoice)} loses to ${convertToWord(computerChoice)}. You Lost!`;
 }
 
-function draw() {
-    console.log("draw")
+function draw(usersChoice, computerChoice) {
+  userScoreSpan.innerHTML = userScore;
+  compScoreSpan.innerHTML = compScore;
+  results.innerHTML = `${convertToWord(usersChoice)} equals ${convertToWord(computerChoice)}. It's a Draw!`;
 }
 
 function game(usersChoice) {
   const computerChoice = getComputerChoice();
   switch (usersChoice + computerChoice) {
-    case "scissorspaper":
-    case "scissorslizard":
-    case "paperrock":
-    case "paperspock":
-    case "rockscissors":
-    case "rocklizard":
-    case "lizardspock":
-    case "lizardpaper":
-    case "spockscissors":
-    case "spockrock":
-        win();
+    case "sp":
+    case "sl":
+    case "pr":
+    case "psp":
+    case "rs":
+    case "rl":
+    case "lsp":
+    case "lp":
+    case "sps":
+    case "spr":
+        win(usersChoice, computerChoice);
         break;
-    case "paperscissors":
-    case "lizardscissors":
-    case "rockpaper":
-    case "spockpaper":
-    case "scissorsrock":
-    case "lizardrock":
-    case "spocklizard":
-    case "paperlizard":
-    case "scissorsspock":
-    case "rockspock":
-        loose();
+    case "ps":
+    case "ls":
+    case "rp":
+    case "spp":
+    case "sr":
+    case "lr":
+    case "spl":
+    case "pl":
+    case "ssp":
+    case "rsp":
+        loose(usersChoice, computerChoice);
         break;
-    case "scissorsscissors":
-    case "paperpaper":
-    case "rockrock":
-    case "lizardlizard":
-    case "spockspock":
-        draw();
+    case "ss":
+    case "pp":
+    case "rr":
+    case "ll":
+    case "spsp":
+        draw(usersChoice, computerChoice);
         break;
   }
 }
 
 function main() {
     rockDiv.addEventListener('click', function() {
-        game("rock");
+        game("r");
     })
     paperDiv.addEventListener('click', function() {
-        game("paper");
+        game("p");
     })
     scissorsDiv.addEventListener('click', function() {
-        game("scissors");
+        game("s");
     })
     spockDiv.addEventListener('click', function() {
-        game("spock");
+        game("sp");
     })
     lizardDiv.addEventListener('click', function() {
-        game("lizard");
+        game("l");
     })
 }
 
